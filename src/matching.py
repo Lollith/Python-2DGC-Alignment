e164bf3820f2b5871df326a547f8f383f8442852
lollith <adeline.gouet@gmail.com>
2025-10-06 15:10:32 +0200

    clean

--- src/matching.py
+++ src/matching.py
@@ -214,7 +214,7 @@ def matching_nist_lib_from_chromato_cube(
                 list_hit = nist_api.hit_list_from_nist_api(results)
                 top_hits = filter_best_hits(list_hit, match_factor_min)
                 cache[spectrum_hash] = top_hits
-                print(f"Peak {i + 1} has {len(top_hits)} hits for {coord}.")
+                # print(f"Peak {i + 1} has {len(top_hits)} hits for {coord}.")
         else:
             top_hits = []
 
